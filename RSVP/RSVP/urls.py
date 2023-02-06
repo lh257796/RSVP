589cbdfafcae757711a38bfb28a887a1995a1667
Han <hl2341@nyu.edu>
2023-02-06 12:05:32 -0500

    change directory files

--- RSVP/RSVP/urls.py
+++ RSVP/RSVP/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('RSVP/', include('RSVP_App.urls')),
+    path('RSVP/', include('RSVP_system.urls')),
     path('admin/', admin.site.urls),
 ]
--- RSVP/RSVP_system/models.py
+++ RSVP/RSVP_system/models.py
@@ -2,19 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class Event(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
-# start, end time; venue
-
 class Venue(models.Model):
     venue_name = models.CharField(max_length=45)
 
 # name
 
+class Event(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    start_time = models.DateTimeField('Event Start Time')
+    end_time = models.DateTimeField('Event End Time')
+
+# start, end time; venue
+
+
 
 class Reservation(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=45)
