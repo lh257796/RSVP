85550cbc8294a81e1d824fe1236719a443cd8be6
Han <hl2341@nyu.edu>
2023-02-10 11:46:37 -0500

    cleanup

--- RSVP/RSVP_system/admin.py
+++ RSVP/RSVP_system/admin.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Venue, Event, Reservation
 
-
-class ReservationInLine(admin.StackedInline):
-    model = Reservation
-    extra = 4
+# class ReservationInLine(admin.StackedInline):
+#     model = Reservation
+#     extra = 4
 
 class EventInLine(admin.StackedInline):
     model = Event
@@ -16,11 +15,9 @@ class EventInLine(admin.StackedInline):
 # class EventAdmin(admin.ModelAdmin):
 #     inlines = [ReservationInLine]
 
-
 class VenueAdmin(admin.ModelAdmin):
     fields = ['venue_name']
     inlines = [EventInLine]
-
 
 admin.site.register(Venue, VenueAdmin)
 admin.site.register(Event)
